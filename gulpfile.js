c728c941952d8548a06729012ffcecf1d454a592
Marco Franceschi <marcofranceschi@hotmail.com>
2017-11-24 10:58:14 -0400

    Setup gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const open = require('gulp-open');
+const browserify = require('browserify');
+const reactify = require('reactify');
+const source = require('vinyl-source-stream');
+
+const config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+
+
+gulp.task('connect', () => {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+
+});
+
+gulp.task('open', ['connect'], () => {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload())
+});
+
+gulp.task('js', ()=> {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.log.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(`${config.paths.dist}/scripts`))
+        .pipe(connect.reload());
+})
+
+gulp.task('html', ()=> {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js'])
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
+
