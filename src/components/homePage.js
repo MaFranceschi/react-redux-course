9e708fe04ff5255d44a12ff002e627655e8c8fb4
Marco Franceschi <marcofranceschi@hotmail.com>
2017-11-24 16:29:11 -0400

    Adding main components

---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const React = require('react');
+
+const Home = React.createClass({
+    render: () => {
+        return (
+            <div className="jumbotron">
+                <h1>Plural Portal</h1>
+                <p>Course react, redux</p>
+            </div>
+        )
+    }
+});
+
+module.exports = Home;
